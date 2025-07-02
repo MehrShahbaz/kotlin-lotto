b999ec2d5921d0dd7f88e09ca8802690fa39ce52
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-02 10:59:45 +0200

    refactor(LottoMachineTest)

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -2,16 +2,14 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMachineTest {
-    @Test
-    fun `Purchase amount should be minimum 1_000 and (maximum 20_000 KRW)`() {
-        assertThrows<IllegalArgumentException> { LottoMachine(0) }
-    }
-
-    @Test
-    fun `2Purchase amount should be minimum 1_000 and (maximum 20_000 KRW)`() {
-        assertThrows<IllegalArgumentException> { LottoMachine(21000) }
+    @ParameterizedTest
+    @ValueSource(ints = [-1000, 0, 21000])
+    fun `Purchase amount should be minimum 1_000 and (maximum 20_000 KRW)`(number: Int) {
+        assertThrows<IllegalArgumentException> { LottoMachine(number) }
     }
 }
 
