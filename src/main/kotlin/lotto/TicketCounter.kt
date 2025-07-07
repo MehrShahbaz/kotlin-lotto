c089aba6506aaf06633c20cbfb6ded9dffafa286
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-07 14:35:40 +0200

    feat(TicketCounter): Create a data class for handling ticket Counts

---
+++ src/main/kotlin/lotto/TicketCounter.kt
@@ -0,0 +1,18 @@
+package lotto
+
+data class TicketCounter(
+    val purchaseAmount: PurchaseAmount,
+    val enteredTicketCount: EnteredTicketCount,
+) {
+    var generatedTicketCount = 0
+
+    init {
+        var purchasableTickets = purchaseAmount.amount / LottoMachine.TICKET_PRICE
+        require(enteredTicketCount.count <= purchasableTickets) { COUNT_ERROR_MESSAGE }
+        generatedTicketCount = purchasableTickets - enteredTicketCount.count
+    }
+
+    companion object {
+        private const val COUNT_ERROR_MESSAGE = "Count should be less than Purchasable Tickets"
+    }
+}
--- src/test/kotlin/lotto/TicketCounterTest.kt
+++ src/test/kotlin/lotto/TicketCounterTest.kt
@@ -7,22 +7,23 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class TicketCounterTest {
-    @Test
-    fun `Manual ticket count should be less than generated tickets`() {
-        assertThrows<IllegalArgumentException> { TicketCounter(3, 4) }
-    }
-
-    @Test
-    fun `Manual ticket count must be positive`() {
-        assertThrows<IllegalArgumentException> { TicketCounter(-1, 3) }
-    }
-
     @ParameterizedTest
-    @ValueSource(ints = [0, 3])
-    fun `Manual ticket count should be less than generated tickets`(number: Int) {
+    @ValueSource(ints = [0, 4, 9])
+    fun `Entered Ticket count is less than equal too Purchasable tickets`(number: Int) {
+        val purchaseAmount = PurchaseAmount(9876)
+        val enteredTicketCount = EnteredTicketCount(number)
         assertEquals(
-            TicketCounter(3, number).enteredTickets,
-            number,
+            TicketCounter(purchaseAmount, enteredTicketCount).purchaseAmount,
+            purchaseAmount,
         )
+    }
+
+    @Test
+    fun `Entered Ticket count should be less than Purchasable tickets`() {
+        val purchaseAmount = PurchaseAmount(9876)
+        val enteredTicketCount = EnteredTicketCount(10)
+        assertThrows<IllegalArgumentException> {
+            TicketCounter(purchaseAmount, enteredTicketCount).purchaseAmount
+        }
     }
 }
