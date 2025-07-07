c7d2a1eaad0a383b21467ecb833595948258fdd6
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-07 11:35:20 +0200

    refactor(Lotto): Move the logic to OutputView

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,10 +6,6 @@ class Lotto(val numbers: List<LottoNumber>) {
         require(numbers.distinct().size == LOTTO_SIZE)
     }
 
-    fun sortedList(): List<Int> {
-        return numbers.map(LottoNumber::value).sorted()
-    }
-
     companion object {
         const val LOTTO_SIZE = 6
     }
