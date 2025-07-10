0c4022729f0e6cca3200a80df571c211f504e2c8
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-10 17:45:40 +0200

    refactor(Lotto): Convert Lotto to Set

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto
 
-class Lotto(val numbers: List<LottoNumber>) {
+class Lotto(numbers: List<LottoNumber>) {
+    val numbers = numbers.toSet()
+
     init {
         require(numbers.size == LOTTO_SIZE) { "List should have exactly 6 numbers" }
-        require(numbers.distinct().size == LOTTO_SIZE) { "All the numbers in the list must be unique" }
+        require(this.numbers.size == numbers.size) { "All the numbers in the list must be unique" }
     }
 
     companion object {
